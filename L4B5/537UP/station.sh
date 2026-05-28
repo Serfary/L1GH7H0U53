9a1974d8e1b9e2c82e76f37dae6a65ba62dff1f9
53RF4RY <serfary@bsidesmaine.org>
2026-05-28 18:25:01 -0400

    Station Service

--- L4B5/537UP/station.sh
+++ L4B5/537UP/station.sh
@@ -3,6 +3,7 @@ apt install git hashcat screen fcrackzip qtbase5-dev -y
 if [ -d "L1GH7H0U53" ]; then
     sudo -u serfary git -C /home/serfary/L1GH7H0U53 fetch --all
     sudo -u serfary git -C /home/serfary/L1GH7H0U53 reset --hard origin/main
+    sudo -u serfary git -C /home/serfary/L1GH7H0U53 clean -fd
     sudo -u serfary git -C /home/serfary/L1GH7H0U53 pull
 else
     sudo -u serfary git clone https://github.com/Serfary/L1GH7H0U53.git
@@ -11,6 +12,7 @@ fi
 cp /home/serfary/L1GH7H0U53/L4B5/537UP/pm3 /usr/local/bin/
 cp /home/serfary/L1GH7H0U53/L4B5/537UP/proxmark3 /usr/local/bin/
 cp /home/serfary/L1GH7H0U53/L4B5/537UP/wifi-check.cron /etc/cron.d/
+cp /home/serfary/L1GH7H0U53/L4B5/537UP/station.service /etc/systemd/system/
 
 chmod +x /home/serfary/L1GH7H0U53/L4B5/537UP/wifi-check.sh
 
--- L4B5/537UP/wifi-check.sh
+++ L4B5/537UP/wifi-check.sh
@@ -6,8 +6,4 @@ then
     sleep 5
     sudo ip link set wlan0 up
     sleep 5
-    sudo -u serfary git -C /home/serfary/L1GH7H0U53 fetch --all
-    sudo -u serfary git -C /home/serfary/L1GH7H0U53 reset --hard origin/main
-    sudo -u serfary git -C /home/serfary/L1GH7H0U53 clean -fd
-    sudo -u serfary git -C /home/serfary/L1GH7H0U53 pull
 fi
