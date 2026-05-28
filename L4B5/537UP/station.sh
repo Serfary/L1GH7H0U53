0476b1e3fb9a46356f192cad5e4e16d39dabf858
53RF4RY <serfary@bsidesmaine.org>
2026-05-28 16:48:25 -0400

    Updating permissions

--- L4B5/537UP/station.sh
+++ L4B5/537UP/station.sh
@@ -1,9 +1,11 @@
 apt update
 apt install git hashcat screen fcrackzip qtbase5-dev -y
 if [ -d "L1GH7H0U53" ]; then
-    git -C "L1GH7H0U53" pull
+    sudo -u serfary git -C /home/serfary/L1GH7H0U53 fetch --all
+    sudo -u serfary git -C /home/serfary/L1GH7H0U53 reset --hard origin/main
+    sudo -u serfary git -C /home/serfary/L1GH7H0U53 pull
 else
-    git clone https://github.com/Serfary/L1GH7H0U53.git
+    sudo -u serfary git clone https://github.com/Serfary/L1GH7H0U53.git
 fi
 
 cp /home/serfary/L1GH7H0U53/L4B5/537UP/pm3 /usr/local/bin/
