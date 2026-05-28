5ab5549b57c2a409c7ac245f637fffa907d2725c
53RF4RY <serfary@bsidesmaine.org>
2026-05-28 08:51:09 -0400

    Station Setup

--- L4B5/537UP/station.sh
+++ L4B5/537UP/station.sh
@@ -1,7 +1,7 @@
 apt update
-apt install git hashcat screen build-essential pkg-config libreadline-dev gcc-arm-none-eabi libnewlib-dev qtbase5-dev -y
+apt install git hashcat screen qtbase5-dev -y
 git clone https://github.com/Serfary/L1GH7H0U53.git
-git clone https://github.com/RfidResearchGroup/proxmark3.git
-cd proxmark3
-make clean && make all
-make install
+
+cp ./pm3 /usr/local/bin/
+cp ./proxmark3 /usr/local/bin/
+cp ./zip2john /usr/local/bin/
